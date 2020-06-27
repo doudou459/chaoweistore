0c88c6ff152a1012b6980e628f02bdd5a91ca30d
doudou459 <459349489@qq.com>
2020-06-27 22:42:16 +0800

    2020-6-27

--- src/views/appointment.vue
+++ src/views/appointment.vue
@@ -13,8 +13,8 @@
               @change="dayTimeChange"
             ></el-date-picker>
           </el-col>
-          <el-col :span="4">
-            <el-popover placement="bottom" title="新增预约" width="400" trigger="click">
+          <el-col :span="4" v-if="dayTime>=new Date(new Date().toLocaleDateString()).getTime()">
+            <el-popover placement="bottom" title="新增预约" width="400" v-model="visible">
               <el-form label-width="90px">
                 <el-form-item label="客户姓名">
                   <el-input placeholder="请输入客户姓名" v-model="newAppointmentOrderData.customerName"></el-input>
@@ -32,14 +32,14 @@
                     ></el-option>
                   </el-select>
                 </el-form-item>
-                <el-form-item label="预约时间"  v-if="newAppointmentOrderData.itemId">
+                <el-form-item label="预约时间" v-if="newAppointmentOrderData.itemId">
                   <el-time-select
                     v-model="newAppointmentOrderData.time"
                     :picker-options="getTimePickeOption"
                   ></el-time-select>
                 </el-form-item>
                 <el-form-item>
-                  <el-button type="success">确定预约</el-button>
+                  <el-button type="success" @click="addNewAppointmentOrder">确定预约</el-button>
                 </el-form-item>
               </el-form>
               <el-button slot="reference">新增预约</el-button>
@@ -134,6 +134,7 @@ export default {
           "customerId",
           "customerName",
           "phone",
+          "itemId",
           "itemName",
           "appointmentTime",
           "openid_min",
@@ -161,13 +162,26 @@ export default {
         customerName: "",
         phone: "",
         itemId: "",
+        itemName: "",
         time: ""
-      }
+      },
+      visible: false
     };
   },
   methods: {
     dayTimeChange: function() {
+      if (!this.dayTime) {
+        return;
+      }
       let me = this;
+      me.getItems();
+      this.newAppointmentOrderData = {
+        customerName: "",
+        phone: "",
+        itemId: "",
+        itemName: "",
+        time: ""
+      };
       this.$ajax({
         url: "/service/admin/getAppointmentOrder",
         method: "post",
@@ -188,7 +202,6 @@ export default {
               for (var i = h; i < 24; i++) {
                 me.hours.push(i);
               }
-              me.getItems();
             } else {
               me.appointmentOrderData.clear();
               me.hours = [];
@@ -207,7 +220,13 @@ export default {
     },
     formTime: function(time) {
       var dateTime = new Date(time);
-      return dateTime.getHours() + ":" + dateTime.getMinutes();
+      return (
+        dateTime.getHours() +
+        ":" +
+        (dateTime.getMinutes() > 10
+          ? dateTime.getMinutes()
+          : "0" + dateTime.getMinutes())
+      );
     },
     isOverTime: function(time) {
       return time - new Date().getTime() < 0;
@@ -216,31 +235,113 @@ export default {
       return new Date().getTime() - time < 86400000;
     },
     getItems: function() {
+      this.items = [];
       var today = new Date(new Date().toLocaleDateString()).getTime();
       var disDay = parseInt((this.dayTime - today) / 86400000);
       this.items = this.appointmentItem.datas.filter(function(item) {
         return item.advanceDay > disDay;
       });
     },
-
+    addNewAppointmentOrder: function() {
+      if (!this.newAppointmentOrderData.customerName) {
+        this.$message({
+          showClose: true,
+          message: "请输入客户姓名",
+          type: "error"
+        });
+        return;
+      }
+      if (!this.newAppointmentOrderData.phone) {
+        this.$message({
+          showClose: true,
+          message: "请输入客户联系电话",
+          type: "error"
+        });
+        return;
+      }
+      if (!this.newAppointmentOrderData.itemId) {
+        this.$message({
+          showClose: true,
+          message: "请选择预约项目",
+          type: "error"
+        });
+        return;
+      }
+      if (!this.newAppointmentOrderData.time) {
+        this.$message({
+          showClose: true,
+          message: "请选择预约时间",
+          type: "error"
+        });
+        return;
+      }
+      let me = this;
+      var selectDate = new Date(this.dayTime);
+      var YY = selectDate.getFullYear() + "-";
+      var MM =
+        (selectDate.getMonth() + 1 < 10
+          ? "0" + (selectDate.getMonth() + 1)
+          : selectDate.getMonth() + 1) + "-";
+      var DD =
+        selectDate.getDate() < 10
+          ? "0" + selectDate.getDate()
+          : selectDate.getDate();
+      var appointmentTime = new Date(
+        YY + MM + DD + " " + this.newAppointmentOrderData.time + ":00"
+      ).getTime();
+      var newOrder = {
+        customerName: this.newAppointmentOrderData.customerName,
+        phone: this.newAppointmentOrderData.phone,
+        itemId: this.newAppointmentOrderData.itemId,
+        itemName: this.appointmentItem.getValueByID(
+          "itemName",
+          this.newAppointmentOrderData.itemId
+        ),
+        appointmentTime: appointmentTime
+      };
+      this.$ajax({
+        url: "/service/admin/addNewAppointmenOrder",
+        method: "post",
+        data: newOrder,
+        headers: {
+          chaoweiStoreToken: me.$store.state.loginToken
+        }
+      })
+        .then(function(res) {
+          if (res.data.errno == "200") {
+            me.dayTimeChange();
+            me.visible = false;
+          } else {
+            me.$message({
+              showClose: true,
+              message: res.data.errmsg,
+              type: "error"
+            });
+          }
+        })
+        .catch(function(error) {
+          me.$message({
+            showClose: true,
+            message: error,
+            type: "error"
+          });
+        });
+    }
   },
-  computed:{
+  computed: {
     getTimePickeOption: function() {
       var itemId = this.newAppointmentOrderData.itemId;
       if (itemId) {
-        var itemStarTime = this.appointmentItem.getValueByID(
-          "startTime",
-          itemId
-        )+"";
-        var itemEndTime = this.appointmentItem.getValueByID(
-          "closeTime",
-          itemId
-        )+"";
-        var itemStep = this.appointmentItem.getValueByID("taktTime", itemId)+"";
+        var itemStarTime =
+          this.appointmentItem.getValueByID("startTime", itemId) + "";
+        var itemEndTime =
+          this.appointmentItem.getValueByID("closeTime", itemId) + "";
+        var itemStep =
+          this.appointmentItem.getValueByID("taktTime", itemId) + "";
         return {
-          start: itemStarTime.substring(0,itemStarTime.length-3),
+          start: itemStarTime.substring(0, itemStarTime.length - 3),
           step: "00:" + itemStep,
-          end: itemEndTime.substring(0,itemEndTime.length-3)
+          end: itemEndTime.substring(0, itemEndTime.length - 3)
         };
       } else {
         return {
